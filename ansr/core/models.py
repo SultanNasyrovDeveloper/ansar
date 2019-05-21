248d4f9a44af9b293b88dccbc71f21bd255116c2
Sultan Nasyrov <tightbtctrader@gmail.com>
2019-05-21 14:14:04 +0300

    Create fields for fonts in settings model. Create font classes. Add fonts to index page

--- ansr/core/models.py
+++ ansr/core/models.py
@@ -9,4 +9,13 @@ class SiteSettings(models.Model):
     email = models.CharField(max_length=50, null=True, blank=True, verbose_name='Электронная почта организации')
     site_email = models.CharField(max_length=50, null=True, blank=True, verbose_name='Электронная почта сайта',
                                   help_text='Почта для оповещений с сайта')
+    font_page_header = models.FileField(upload_to='fonts/', null=True, blank=True,
+                                        verbose_name='Шрифт заголовка страницы')
+    font_block_header = models.FileField(upload_to='fonts/', null=True, blank=True,
+                                         verbose_name='Шрифт заголовка блоков')
+    font_product_header = models.FileField(upload_to='fonts/', null=True, blank=True,
+                                           verbose_name='Шрифт заголовка товаров')
+    font_additional = models.FileField(upload_to='fonts/', null=True, blank=True,
+                                       verbose_name='Шрифт дополнительных текстов')
+
 
